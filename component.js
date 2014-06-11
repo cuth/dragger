baec6d4bf9d421c563cf5f5f17047ad40e2daba7
Jon Cuthbert <jon@cuth.net>
2014-06-10 22:56:32 -0400

    add component

---
+++ component.js
@@ -0,0 +1,11 @@
+{
+    "name": "dragger",
+    "repo": "cuth/dragger",
+    "version": "1.2.1",
+    "scripts": ["dist/jquery.dragger.js"],
+    "author": "Jonathan Cuthbert",
+    "license": "MIT",
+    "dependencies": {
+        "jquery/jquery": "*"
+    }
+}
